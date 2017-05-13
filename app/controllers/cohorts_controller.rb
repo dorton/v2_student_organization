d8aec91eca5b6ebbf904a7a3f959246738468b7b
Brian Dorton <dorton@gmail.com>
2017-05-13 06:49:35 -0500

    adds cohort new and edit views and actions

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,6 +1,43 @@
 class CohortsController < ApplicationController
+
+
+
   def show
-    @cohort = Cohort.find(params[:id])
+    @cohort = current_user.cohorts.find(params[:id])
     @days = @cohort.days.all
+  end
+
+  def new
+    @cohort = current_user.city.cohorts.new
+  end
+
+  def edit
+    @cohort = current_user.cohorts.find(params[:id])
+  end
+
+  def create
+    @cohort = current_user.city.cohorts.new(cohort_params)
+
+    @cohort.users << current_user.city.users
+
+    if @cohort.save!
+      redirect_to cohort_path(@cohort)
+    else
+      redirect_to new_cohort_path
+    end
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+  private
+
+  def cohort_params
+    params.require(:cohort).permit(:start_date, :end_date, :course_name, )
   end
 end
