337efd3f6c07636213ab73eba4f6ae73a93e9598
Brian Dorton <dorton@gmail.com>
2017-05-12 22:26:38 -0500

    refactors group form. adds an edit and destroy action on groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,22 @@
 class GroupsController < ApplicationController
 
+  def new
+    @cohort = Cohort.find(params[:cohort_id])
+    @day = Day.find(params[:day_id])
+    @group = @day.groups.new
+  end
+
+  def edit
+    @cohort = Cohort.find(params[:cohort_id])
+    @day = @cohort.days.find(params[:day_id])
+    @group = @day.groups.find(params[:id])
+
+  end
+
   def create
     @cohort = Cohort.find(params[:cohort_id])
     @day = Day.find(params[:day_id])
-    @group = @day.groups.new(day_params)
+    @group = @day.groups.new(group_params)
 
 
     if @group.save!
@@ -14,9 +27,29 @@ class GroupsController < ApplicationController
 
   end
 
+  def update
+    @cohort = Cohort.find(params[:cohort_id])
+    @day = @cohort.days.find(params[:day_id])
+    @group = @day.groups.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to cohort_day_path(@cohort, @day), notice: "hizaugh!"
+    else
+      redirect_to cohort_day_path(@cohort, @day), notice: "oooops!"
+    end
+  end
+
+  def destroy
+    @cohort = Cohort.find(params[:cohort_id])
+    @day = @cohort.days.find(params[:day_id])
+
+    @day.groups.find(params[:id]).destroy
+    redirect_to cohort_day_path(@cohort, @day), notice: "Demoed!"
+  end
+
     private
 
-    def day_params
+    def group_params
       params.require(:group).permit(:name, :user_id, :start_time, :end_time, :description, :review, :activity_level, :one_on_one, :ai_session)
     end
 end
