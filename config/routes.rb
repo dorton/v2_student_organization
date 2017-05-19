3688dc389df6804e408d1a6a6b0a793791b47dc4
Brian Dorton <dorton@gmail.com>
2017-05-18 22:13:00 -0500

    updates devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -32,10 +32,7 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users, controllers: { registrations: "registrations"}
-    as :user do
-      get '/' => 'devise/registrations#new'
-    end
+  devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
