399f99d8dce0fc52bb465d5bc46dc512ee013bd6
Brian Dorton <dorton@gmail.com>
2017-05-20 12:35:52 -0500

    automatically adds a group for all everyday activities when creating a new day

--- app/models/group.rb
+++ app/models/group.rb
@@ -13,4 +13,33 @@ class Group < ApplicationRecord
     clone [:student_groups, :user_groups, :group_tivities]
   end
 
+
+  def self.create_everyday_groups(day)
+    activities = Activity.where(everyday: true)
+    activities.each do |activity|
+      group = day.groups.new
+      group.add_all_students = true
+      group.campus_area = CampusArea.find_by(name: "Main Room")
+      if activity.name == "Morning Announcements"
+        group.start_time = "09:30AM"
+      elsif activity.name == "Lecture Review"
+        group.start_time = "10:30AM"
+      elsif activity.name == "Assessed Project"
+        group.start_time = "1:00PM"
+      elsif activity.name == "Open Lab"
+        group.start_time = "2:00PM"
+      elsif activity.name == "AI Assessment and Check In"
+        group.start_time = "4:00PM"
+      else
+        group.start_time = "09:30AM"
+      end
+      group.save
+
+      group.students << day.cohort.students unless activity.name == "Lecture Review"
+      group.activities << activity
+
+    end
+
+  end
+
 end
