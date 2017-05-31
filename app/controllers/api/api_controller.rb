a964d2a313967f6f96b167a213d7025d972bc230
Brian Dorton <dorton@gmail.com>
2017-05-31 05:36:12 -0500

    removes 'authenticate student' from api controller. don't need it with doorkeeper.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -4,14 +4,7 @@ class API::ApiController < ActionController::Base
   before_action do
     @current_student = Student.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
-  #
-  # def authenticate_student!
-  #   if @current_student.nil?
-  #     session[:redirect_to] = request.path # /products
-  #     redirect_to api_sign_in_path
-  #   end
-  # end
-  #
+
   def current_student
     @current_student
   end
